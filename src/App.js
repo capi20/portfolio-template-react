f3219783a0b26dc85d5c9a1c5a81f4197c2d8cfb
shyam <shyam20998@gmail.com>
2022-06-09 07:47:39 +0530

    created navbar layout

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ import Skills from './containers/skills'
 import Resume from './containers/resume'
 import Portfolio from './containers/portfolio'
 import Contact from './containers/contact'
+import Navbar from './components/navBar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Routes>
         <Route path="/" index element={<Home/>}/>
-        <Route path="/about" index element={<About/>}/>
-        <Route path="/skills" index element={<Skills/>}/>
-        <Route path="/resume" index element={<Resume/>}/>
-        <Route path="/portfolio" index element={<Portfolio/>}/>
-        <Route path="/contact" index element={<Contact/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/skills" element={<Skills/>}/>
+        <Route path="/resume" element={<Resume/>}/>
+        <Route path="/portfolio" element={<Portfolio/>}/>
+        <Route path="/contact" element={<Contact/>}/>
       </Routes>
     </div>
   );
