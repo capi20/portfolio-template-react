a6dac4716241d2c593d7a5de6cfa3b0fdbef7e03
shyam <shyam20998@gmail.com>
2022-06-10 07:02:43 +0530

    added particles js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom'
-import Home from './containers/home'
-import About from './containers/about'
-import Skills from './containers/skills'
-import Resume from './containers/resume'
-import Portfolio from './containers/portfolio'
-import Contact from './containers/contact'
-import Navbar from './components/navBar';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import Home from "./containers/home";
+import About from "./containers/about";
+import Skills from "./containers/skills";
+import Resume from "./containers/resume";
+import Portfolio from "./containers/portfolio";
+import Contact from "./containers/contact";
+import Navbar from "./components/navBar";
+import particlesConfig from "./components/helpers/particlesConfig";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-      <Routes>
-        <Route path="/" index element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/skills" element={<Skills/>}/>
-        <Route path="/resume" element={<Resume/>}/>
-        <Route path="/portfolio" element={<Portfolio/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-      </Routes>
-    </div>
-  );
+	const particlesInit = async (main) => {
+		await loadFull(main);
+	};
+
+	return (
+		<div className="App">
+			<Particles
+				id="particles"
+				options={particlesConfig}
+				init={particlesInit}
+			/>
+			<Navbar />
+			<Routes>
+				<Route path="/" index element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/skills" element={<Skills />} />
+				<Route path="/resume" element={<Resume />} />
+				<Route path="/portfolio" element={<Portfolio />} />
+				<Route path="/contact" element={<Contact />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
