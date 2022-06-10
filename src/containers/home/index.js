67012ffb57091b2bb999f03759c1ca8b4cf641ae
shyam <shyam20998@gmail.com>
2022-06-10 07:56:48 +0530

    added buttons and social icons to home page

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaFacebook, FaLinkedin, FaInstagram, FaTwitter } from "react-icons/fa";
 import "./styles.scss";
 
 const Home = () => {
@@ -6,10 +7,22 @@ const Home = () => {
 		<section className="home" id="home">
 			<div className="home__text-wrapper">
 				<h1>
-					Hello, I'm Shyam Ji Gupta
+					Hello, I'm Shyam
 					<br />
-					Front End Developer.
+					Front End Developer
 				</h1>
+			</div>
+			<div className="contact-me">
+				<div className="contact-me__buttons-wrapper">
+					<button>Hire me</button>
+					<a>Download resume</a>
+				</div>
+				<div className="contact-me__socials-wrapper">
+					<FaLinkedin size={32} />
+					<FaFacebook size={32} />
+					<FaTwitter size={32} />
+					<FaInstagram size={32} />
+				</div>
 			</div>
 		</section>
 	);
