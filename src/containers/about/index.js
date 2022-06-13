b455ec06537ed65aa569853a35e486ffb07c51c2
shyam <shyam20998@gmail.com>
2022-06-13 07:53:27 +0530

    created about page

--- src/containers/about/index.js
+++ src/containers/about/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { BsInfoCircleFill } from "react-icons/bs";
 import PageHeaderContent from "../../components/pageHeaderContent";
+import { DiApple, DiAndroid } from "react-icons/di";
+import { FaDev, FaDatabase } from "react-icons/fa";
+import { personaldata } from "./utils";
+import "./styles.scss";
 
 const About = () => {
 	return (
@@ -9,6 +13,49 @@ const About = () => {
 				headerText="About me"
 				icon={<BsInfoCircleFill size={40} />}
 			/>
+			<div className="about__content">
+				<div className="about__content__personalWrapper">
+					<h3 className="developerContent">Front end developer</h3>
+					<p>
+						I completed my B.Tech in Computer Science & Engineering from Jaypee
+						Institute of Information Technology, Noida. I'm a passionate
+						front-end developer with 2+ years of expercience in front end
+						development. I am currently working as a associate technology L2 at
+						Publicis Sapient. I aspire to create web applications that not only
+						operates efficiently behind the scene, but also provides rich user
+						experiences. I love learning new and better ways to create seamless
+						user experiences with clean, efficient, and scalable code. For me
+						work is an ongoing education, and work place is an institute where
+						every one has something to share. Besides of the professional
+						things, I like to read books and watch movies in my free time.
+					</p>
+					<h3 className="personalContent">Personal Information</h3>
+					<ul>
+						{personaldata.map((item, key) => (
+							<li key={key}>
+								<span className="title">{item.label}</span>
+								<span className="value">{item.value}</span>
+							</li>
+						))}
+					</ul>
+				</div>
+				<div className="about__content__servicesWrapper">
+					<div className="about__content__servicesWrapper__innerContent">
+						<div>
+							<DiApple size={60} color="var(--selected-theme-main-color)" />
+						</div>
+						<div>
+							<FaDatabase size={60} color="var(--selected-theme-main-color)" />
+						</div>
+						<div>
+							<DiAndroid size={60} color="var(--selected-theme-main-color)" />
+						</div>
+						<div>
+							<FaDev size={60} color="var(--selected-theme-main-color)" />
+						</div>
+					</div>
+				</div>
+			</div>
 		</section>
 	);
 };
---
+++ src/containers/about/utils.js
@@ -0,0 +1,22 @@
+export const personaldata = [
+	{
+		label: "Name",
+		value: "Shyam Ji Gupta"
+	},
+	{
+		label: "Age",
+		value: "23"
+	},
+	{
+		label: "Address",
+		value: "India"
+	},
+	{
+		label: "Email",
+		value: "a@a.com"
+	},
+	{
+		label: "Contact No",
+		value: "+91 7123456789"
+	}
+];
