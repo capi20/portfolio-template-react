7b227fa65eaf4b7afda46517d3752c22731050bb
shyam <shyam20998@gmail.com>
2022-06-13 08:04:39 +0530

    added animation to about page

--- src/containers/about/index.js
+++ src/containers/about/index.js
@@ -15,29 +15,34 @@ const About = () => {
 			/>
 			<div className="about__content">
 				<div className="about__content__personalWrapper">
-					<h3 className="developerContent">Front end developer</h3>
-					<p>
-						I completed my B.Tech in Computer Science & Engineering from Jaypee
-						Institute of Information Technology, Noida. I'm a passionate
-						front-end developer with 2+ years of expercience in front end
-						development. I am currently working as a associate technology L2 at
-						Publicis Sapient. I aspire to create web applications that not only
-						operates efficiently behind the scene, but also provides rich user
-						experiences. I love learning new and better ways to create seamless
-						user experiences with clean, efficient, and scalable code. For me
-						work is an ongoing education, and work place is an institute where
-						every one has something to share. Besides of the professional
-						things, I like to read books and watch movies in my free time.
-					</p>
-					<h3 className="personalContent">Personal Information</h3>
-					<ul>
-						{personaldata.map((item, key) => (
-							<li key={key}>
-								<span className="title">{item.label}</span>
-								<span className="value">{item.value}</span>
-							</li>
-						))}
-					</ul>
+					<div className="about__content__personalWrapper--intro">
+						<h3 className="developerContent">Front end developer</h3>
+						<p>
+							I completed my B.Tech in Computer Science & Engineering from
+							Jaypee Institute of Information Technology, Noida. I'm a
+							passionate front-end developer with 2+ years of expercience in
+							front end development. I am currently working as a associate
+							technology L2 at Publicis Sapient. I aspire to create web
+							applications that not only operates efficiently behind the scene,
+							but also provides rich user experiences. I love learning new and
+							better ways to create seamless user experiences with clean,
+							efficient, and scalable code. For me work is an ongoing education,
+							and work place is an institute where every one has something to
+							share. Besides of the professional things, I like to read books
+							and watch movies in my free time.
+						</p>
+					</div>
+					<div className="about__content__personalWrapper--details">
+						<h3 className="personalContent">Personal Information</h3>
+						<ul>
+							{personaldata.map((item, key) => (
+								<li key={key}>
+									<span className="title">{item.label}</span>
+									<span className="value">{item.value}</span>
+								</li>
+							))}
+						</ul>
+					</div>
 				</div>
 				<div className="about__content__servicesWrapper">
 					<div className="about__content__servicesWrapper__innerContent">
