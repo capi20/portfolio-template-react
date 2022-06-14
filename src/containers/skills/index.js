eebf121e9075baea3963c47f21c518e70c4fad3c
shyam <shyam20998@gmail.com>
2022-06-14 07:32:37 +0530

    created skills page layout

--- src/containers/skills/index.js
+++ src/containers/skills/index.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import { GiSkills } from "react-icons/gi";
+import PageHeaderContent from "../../components/pageHeaderContent";
+import { skillsData } from "./utils";
+import { Line } from "rc-progress";
 
 const Skills = () => {
-  return (
-    <div>Skills</div>
-  )
-}
+	return (
+		<section id="skills" className="skills">
+			<PageHeaderContent headerText="My skills" icon={<GiSkills size={40} />} />
+			<div className="skills__content-wrapper">
+				{skillsData.map((item, key) => (
+					<div key={key} className="skills__content-wrapper__inner-content">
+						<h3 className="skills__content-wrapper__inner-content__category">
+							{item.label}
+						</h3>
+						<div>
+							{item.data.map((dataItem, index) => (
+								<div className="progressbar-wrapper" key={index}>
+									<p>{dataItem.skillName}</p>
+									<Line
+										percent={dataItem.percentage}
+										strokeColor="var(--selected-theme-main-color)"
+										strokeWidth="2"
+										strokeLinecap="square"
+										trailWidth="2"
+									/>
+								</div>
+							))}
+						</div>
+					</div>
+				))}
+			</div>
+		</section>
+	);
+};
 
-export default Skills
+export default Skills;
---
+++ src/containers/skills/utils.js
@@ -0,0 +1,23 @@
+export const skillsData = [
+	{
+		label: "FRONT END",
+		data: [
+			{
+				skillName: "HTML",
+				percentage: "90"
+			},
+			{
+				skillName: "CSS",
+				percentage: "90"
+			},
+			{
+				skillName: "JAVASCRIPT",
+				percentage: "80"
+			},
+			{
+				skillName: "REACT",
+				percentage: "60"
+			}
+		]
+	}
+];
