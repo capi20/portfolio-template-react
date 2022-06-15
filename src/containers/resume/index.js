bd4cac9a4adde81ff90583e1596966a78f7d0719
shyam <shyam20998@gmail.com>
2022-06-15 08:16:58 +0530

    styled resume page

--- src/containers/resume/index.js
+++ src/containers/resume/index.js
@@ -8,6 +8,7 @@ import {
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import { data } from "./utils";
+import "./styles.scss";
 
 const Resume = () => {
 	return (
@@ -18,7 +19,7 @@ const Resume = () => {
 			/>
 			<div className="timeline">
 				<div className="timeline__experience">
-					<h3 className="timeline__experience__header--text">Experience</h3>
+					<h3 className="section-heading">Experience</h3>
 					<VerticalTimeline
 						layout="1-column"
 						lineColor="var(--selected-theme-main-color)">
@@ -36,20 +37,49 @@ const Resume = () => {
 									background: "#181818",
 									color: "var(--selected-theme-main-color)"
 								}}
-								icon={<MdWork />}
-								dateClassName="vertical-timeline-element-custom-date">
+								icon={<MdWork />}>
 								<div className="vertical-timeline-element-title-wrapper">
-									<h3 className="vertical-timeline-element-title">
-										{item.title}
-									</h3>
-									<h4 className="vertical-timeline-element">{item.location}</h4>
+									<h3>{item.title}</h3>
+									<h4>{item.subTitle}</h4>
 								</div>
-								<p>{item.description}</p>
+								<p className="vertical-timeline-element-description">
+									{item.description}
+								</p>
 							</VerticalTimelineElement>
 						))}
 					</VerticalTimeline>
 				</div>
-				<div className="timeline__education"></div>
+				<div className="timeline__education">
+					<h3 className="section-heading">Education</h3>
+					<VerticalTimeline
+						layout="1-column"
+						lineColor="var(--selected-theme-main-color)">
+						{data.education.map((item) => (
+							<VerticalTimelineElement
+								key={item.title}
+								className="timeline__education__vt-component"
+								contentStyle={{
+									background: "none",
+									color: "var(--selected-theme-main-color)",
+									border: "1.5px solid var(--selected-theme-main-color)"
+								}}
+								date={item.date}
+								iconStyle={{
+									background: "#181818",
+									color: "var(--selected-theme-main-color)"
+								}}
+								icon={<MdWork />}>
+								<div className="vertical-timeline-element-title-wrapper">
+									<h3>{item.title}</h3>
+									<h4>{item.subTitle}</h4>
+								</div>
+								<p className="vertical-timeline-element-description">
+									{item.description}
+								</p>
+							</VerticalTimelineElement>
+						))}
+					</VerticalTimeline>
+				</div>
 			</div>
 		</section>
 	);
--- src/containers/resume/utils.js
+++ src/containers/resume/utils.js
@@ -2,13 +2,13 @@ export const data = {
 	experience: [
 		{
 			title: "Associate Technology L2",
-			subTitle: "Gurgoan, India",
+			subTitle: "Publicis Sapient, Gurgoan, India",
 			description: "Front end developer",
 			date: "February 2022 - present"
 		},
 		{
 			title: "Survey Programmer",
-			subTitle: "Noida, India",
+			subTitle: "Dynata, Noida, India",
 			description: "Online surveys, data collection",
 			date: "July 2019 - February 2022"
 		}
@@ -17,12 +17,14 @@ export const data = {
 		{
 			title: "B.Tech",
 			subTitle: "Jaypee Institute of Information Technology, Noida, India",
-			description: "2015 - 2019"
+			description: "Computer Science Engineering",
+			date: "2015 - 2019"
 		},
 		{
 			title: "12th",
 			subTitle: "Shri Gopal Inter College, Auraiya, India",
-			description: "2013 - 2014"
+			description: "12th grade in science",
+			date: "2013 - 2014"
 		}
 	]
 };
