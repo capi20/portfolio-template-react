fccf33bd6320be66f9d76fcb9878b2d9fc8dc8bc
shyam <shyam20998@gmail.com>
2022-06-15 07:47:26 +0530

    created resume page layout

--- src/containers/resume/index.js
+++ src/containers/resume/index.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from "react";
+import { FaBlackTie } from "react-icons/fa";
+import { MdWork } from "react-icons/md";
+import PageHeaderContent from "../../components/pageHeaderContent";
+import {
+	VerticalTimeline,
+	VerticalTimelineElement
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { data } from "./utils";
 
 const Resume = () => {
-  return (
-    <div>Resume</div>
-  )
-}
+	return (
+		<section className="resume" id="resume">
+			<PageHeaderContent
+				headerText="My resume"
+				icon={<FaBlackTie size={40} />}
+			/>
+			<div className="timeline">
+				<div className="timeline__experience">
+					<h3 className="timeline__experience__header--text">Experience</h3>
+					<VerticalTimeline
+						layout="1-column"
+						lineColor="var(--selected-theme-main-color)">
+						{data.experience.map((item) => (
+							<VerticalTimelineElement
+								key={item.title}
+								className="timeline__experience__vt-component"
+								contentStyle={{
+									background: "none",
+									color: "var(--selected-theme-main-color)",
+									border: "1.5px solid var(--selected-theme-main-color)"
+								}}
+								date={item.date}
+								iconStyle={{
+									background: "#181818",
+									color: "var(--selected-theme-main-color)"
+								}}
+								icon={<MdWork />}
+								dateClassName="vertical-timeline-element-custom-date">
+								<div className="vertical-timeline-element-title-wrapper">
+									<h3 className="vertical-timeline-element-title">
+										{item.title}
+									</h3>
+									<h4 className="vertical-timeline-element">{item.location}</h4>
+								</div>
+								<p>{item.description}</p>
+							</VerticalTimelineElement>
+						))}
+					</VerticalTimeline>
+				</div>
+				<div className="timeline__education"></div>
+			</div>
+		</section>
+	);
+};
 
-export default Resume
+export default Resume;
---
+++ src/containers/resume/utils.js
@@ -0,0 +1,28 @@
+export const data = {
+	experience: [
+		{
+			title: "Associate Technology L2",
+			subTitle: "Gurgoan, India",
+			description: "Front end developer",
+			date: "February 2022 - present"
+		},
+		{
+			title: "Survey Programmer",
+			subTitle: "Noida, India",
+			description: "Online surveys, data collection",
+			date: "July 2019 - February 2022"
+		}
+	],
+	education: [
+		{
+			title: "B.Tech",
+			subTitle: "Jaypee Institute of Information Technology, Noida, India",
+			description: "2015 - 2019"
+		},
+		{
+			title: "12th",
+			subTitle: "Shri Gopal Inter College, Auraiya, India",
+			description: "2013 - 2014"
+		}
+	]
+};
