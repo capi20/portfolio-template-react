98d185ca6c4971b7cb563fa62b265dbbbaf02483
shyam <shyam20998@gmail.com>
2022-06-17 07:36:31 +0530

    created contact page layout

--- src/containers/contact/index.js
+++ src/containers/contact/index.js
@@ -1,9 +1,44 @@
-import React from 'react'
-
+import React from "react";
+import { RiContactsFill } from "react-icons/ri";
+import PageHeaderContent from "../../components/pageHeaderContent";
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+	return (
+		<section id="contact" className="contact">
+			<PageHeaderContent
+				headerText="Contact me"
+				icon={<RiContactsFill size={40} />}
+			/>
+			<div className="contact__content">
+				<h3 className="section-heading">Let's Talk</h3>
+				<div className="contact__content__form">
+					<div className="nameWrapper">
+						<input name="name" className="inputName" type="text" />
+						<label htmlFor="name" className="nameLabel">
+							Name
+						</label>
+					</div>
+					<div className="emailWrapper">
+						<input name="email" className="inputEmail" type="text" />
+						<label htmlFor="email" className="emailLabel">
+							Email
+						</label>
+					</div>
+					<div className="descriptionWrapper">
+						<textarea
+							name="description"
+							className="inputDescription"
+							type="text"
+							rows={"5"}
+							style={{ resize: "none" }}
+						/>
+						<label htmlFor="description" className="descriptionLabel">
+							Description
+						</label>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
 
-export default Contact
+export default Contact;
